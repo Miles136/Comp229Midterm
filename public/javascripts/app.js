496be7b9b35c91efa28ca99f27f42dda6ccb6fa3
Miles136 <nemanuel.montoya@gmail.com>
2022-03-05 19:45:32 -0500

    Changed app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,20 @@
 console.log('Goes to the client side.');
 
-if(getTitle == "Movie List")
-{
-    let deleteButtons = document.querySelectorAll('.btn-danger');
-        
-    for(button of deleteButtons)
-    {
-        button.addEventListener('click', (event)=>{
-            var id = $(this).attr("data-id");
-            var request = {
-              // Change URL
-              url: `https://FIXTHIS.herokuapp.com/movie/delete/${id}`,
-              method: "DELETE",
-            };
-        
-            if (confirm("Do you really want to delete this record?")) {
-              $.ajax(request).done(function (response) {
-                alert("Data Deleted Successfully!");
-                location.reload();
-              });
-            }
-        });
+if (window.location.pathname == "/movie/list") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+    console.log(id);
+    var request = {
+      url: `https://milesmidterm.herokuapp.com/movie/delete/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
     }
+  });
 }
