b7bcea639f0f3033e8e5501f2170e49b013eebb6
Miles136 <nemanuel.montoya@gmail.com>
2022-03-05 20:34:58 -0500

    attempt 3?

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,17 +4,31 @@
 
 console.log('Goes to the client side.');
 
+
+
 if (window.location.pathname == "/movie/list") {
   $ondelete = $(".table tbody td a.delete");
   $ondelete.click(function () {
-    var id = $(this).attr("data-id");
-    console.log(id);
+    var movieid = $(this).attr("data-id");
+    console.log(movieid);
+    var MongoClient = require( 'mongodb' ).MongoClient;
+    var url = "milesmidterm.herokuapp.com/movie/delete/";
+
+    MongoClient.connect( url , function( err , db ) {
+      if (err) throw err;
+      var dbo = db.db("myFirstDatabase");
+      var query = { id : movieid };
+      dbo.collection("movies").deleteOne(query, function(err, obj) {
+        if (err) throw err;
+        console.log("Deleted successfully");
+      })
+    });
     var request = {
-      url: `https://milesmidterm.herokuapp.com/movie/delete/${id}`,
-      method: "DELETE",
+      url: "https://milesmidterm.herokuapp.com/movie/delete/${id}",
+      method: "DELETE"
     };
 
-    if (confirm("Do you really want to delete this record?")) {
+    if (confirm("Remove this movie from the list?")) {
       $.ajax(request).done(function (response) {
         alert("Data Deleted Successfully!");
         location.reload();
