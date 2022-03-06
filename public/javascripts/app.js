a0caad4e31e4d848bffaedc872d858ee49a5cd68
Miles136 <nemanuel.montoya@gmail.com>
2022-03-05 20:44:28 -0500

    was it a typo this whole time?

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,25 +3,13 @@
 // Student ID: 301247391
 
 console.log('Goes to the client side.');
-var MongoClient = require( 'mongodb' ).MongoClient;
 
 
 if (window.location.pathname == "/movie/list") {
   $ondelete = $(".table tbody td a.delete");
   $ondelete.click(function () {
-    var movieid = $(this).attr("data-id");
-    console.log(movieid);
-    var url = "milesmidterm.herokuapp.com/movie/delete/";
-
-    MongoClient.connect( url , function( err , db ) {
-      if (err) throw err;
-      var dbo = db.db("myFirstDatabase");
-      var query = { id : movieid };
-      dbo.collection("movies").deleteOne(query, function(err, obj) {
-        if (err) throw err;
-        console.log("Deleted successfully");
-      })
-    });
+    var id = $(this).attr("data-id");
+    console.log(id);
     var request = {
       url: "https://milesmidterm.herokuapp.com/movie/delete/${id}",
       method: "DELETE"
